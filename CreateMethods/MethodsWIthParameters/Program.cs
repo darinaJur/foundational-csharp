36f54c2fd4655e1ce9cc15a1b66fc47de538512b
Darina Jurovskaja <darina.jurovskaja@gmail.com>
2024-09-29 16:46:07 +0100

    Add exercise using method with parameters

--- CreateMethods/MethodsWIthParameters/Program.cs
+++ CreateMethods/MethodsWIthParameters/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Exercise - Complete the challenge to display email addresses
+
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    // display internal email addresses
+    ShowEmail(firstName: corporate[i,0], lastName: corporate[i,1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    // display external email addresses
+    ShowEmail(firstName: external[i,0], lastName: external[i,1], domain: externalDomain);
+}
+
+void ShowEmail(string firstName, string lastName, string domain = "contoso.com")
+{
+    string email = firstName.Substring(0, 2) + lastName;
+	Console.WriteLine($"{email}@{domain}".ToLower());
+}
